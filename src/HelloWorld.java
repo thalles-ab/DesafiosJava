046d78bfaef4887aea9daadbd6bbfd4e474cb599
thalles batista <thallesmweb@gmail.com>
2024-02-08 19:47:12 -0300

    commit inicial

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-
     }
 }
---
+++ src/hackhank/basic/JavaStdInStdOutOne.java
@@ -0,0 +1,12 @@
+package hackhank.basic;
+
+import java.util.Scanner;
+
+public class JavaStdInStdOutOne {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        System.out.println(a);
+    }
+}
